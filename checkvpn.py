b09b4fd1a71123738ebab7a0c400cf84284abbc8
toxydose <a.yakovlev911@gmail.com>
2020-06-17 06:42:07 -0400

    bug fix

--- checkvpn.py
+++ checkvpn.py
@@ -5,10 +5,9 @@ message = ''
 output = run('dig +short myip.opendns.com @resolver1.opendns.com', shell=True, stdout=PIPE)
 ip = output.stdout.decode('utf-8')
 
-if 'IP' in ip:
+if IP in ip:
     message = 'real ip is disclosed'
     bot.send_message(chat_id=CHAT_ID, text=(ip + message))
-
 else:
     if len(ip) < 17 or len(ip) > 8:
         pass
