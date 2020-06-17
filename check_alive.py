e64ee4e7647f18f0c77452a678195fcdf0010ea1
Ubuntu <ubuntu@ip-172-31-43-89.us-east-2.compute.internal>
2020-06-17 11:18:09 +0000

    add check_alive script

---
+++ check_alive.py
@@ -0,0 +1,21 @@
+from subprocess import run, PIPE
+import time
+from bot import *
+message = '_'
+command = 'nc ' + IP + ' ' + PORT + ' -w 1'
+output = run(command, shell=True, stdout=PIPE)
+message = output.stdout.decode('utf-8')
+status = False
+
+print (message)
+if 'SSH' in message:
+	status = True
+	# bot.send_message(chat_id=CHAT_ID, text=message)
+
+if status == False:
+	output = run(command, shell=True, stdout=PIPE)
+	if 'SSH' in message:
+		print()
+	else:
+		time.sleep(3)
+		bot.send_message(chat_id=CHAT_ID, text=(HOSTNAME + ' is unreachable ' + message))
