31438a8015e60e06b02819339b6b45f5497d5d1e
Alexander Bridges <a.yakovlev911@gmail.com>
2020-06-23 12:53:13 +0300

    bug fix

--- check_alive.py
+++ check_alive.py
@@ -11,13 +11,12 @@ status = False
 print(message)
 if 'SSH' in message:
     status = True
-# bot.send_message(chat_id=CHAT_ID, text=message)
 
 if status == False:
+    time.sleep(3)
     output = run(command, shell=True, stdout=PIPE)
     message = output.stdout.decode('utf-8')
     if 'SSH' in message:
         print()
     else:
-        time.sleep(3)
         bot.send_message(chat_id=CHAT_ID, text=(REMOTE_HOSTNAME + ' is unreachable ' + message))
